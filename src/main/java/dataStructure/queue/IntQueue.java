b5acc1dffe00a2c3b36f71a31c0f7c7b2c1d232f
Keun Kyeong <woodeekim@gmail.com>
2020-03-17 23:18:10 +0900

    Add dataStructure for queue of int

---
+++ src/main/java/dataStructure/queue/IntQueue.java
@@ -0,0 +1,95 @@
+package dataStructure.queue;
+
+/**
+    - 큐는 FIFO(First In First Out)인 자료구조이다.
+        - 먼저 넣은 데이터를 먼저 꺼낸다.
+    - int Queue 구현
+        - enque, deque, peek, indexOf, clear, capacity,
+           size, isEmpty, isFull, dump 메소드 구현
+**/
+public class IntQueue {
+    private int max;    //큐의 용량
+    private int front;  //첫 번째 요소 커서
+    private int rear;   //마지막 요소 커서
+    private int number; //현재 데이터 수
+    private int[] queue;//큐 본체
+
+    //큐안에 비어있는데 데이터를 꺼낼 경우 예외 처리
+    public class EmptyIntQueueException extends RuntimeException {
+        public EmptyIntQueueException(){}
+    }
+    //큐안에 가득차있는데 데이터를 넣을 경우 예외 처리
+    public class OverflowIntQueueException extends RuntimeException {
+        public OverflowIntQueueException(){}
+    }
+
+    //생성자
+    public IntQueue(int capacity)  {
+        number = front = rear = 0;
+        max = capacity;
+        try {
+            queue = new int[max];
+        }catch (OutOfMemoryError error){
+            max = 0;
+        }
+    }
+
+    //enque
+    public int enque(int x) throws OverflowIntQueueException {
+        if(number >= max) {
+            throw new OverflowIntQueueException();
+        }
+        //인큐가 들어오면 마지막 요소 커서인 rear 에 x가 대입 후 증가
+        queue[rear++] = x;
+        number++;
+
+        //max 와 같을 때 rear 는 0으로 값을 대입해줘야 된다. 안하면 max 보다 초과해서 에러
+        if(rear == max){
+            rear = 0;
+        }
+        return x;
+    }
+
+    //deque()
+    public int deque() throws EmptyIntQueueException {
+        if(number <= 0){
+            throw new EmptyIntQueueException();
+        }
+        //선입선출 구조기 때문에 front 자리가 먼저 나가야된다.
+        //x인 변수이름 말고 좋은 이름이 없을까?
+        int x = queue[front++];
+        number--;
+        if(front == max) {
+            front = 0;
+        }
+
+        return x;
+    }
+
+    public static void main(String[] args) {
+        IntQueue que = new IntQueue(5);
+        System.out.println("max : "+que.max);
+        System.out.println("front : "+que.front);
+        System.out.println("rear : "+que.rear);
+        System.out.println("number : "+que.number);
+
+        que.enque(5);
+        que.enque(7);
+        que.enque(10);
+        que.enque(12 );
+
+        for(int i = que.front; i<que.rear; i++){
+            System.out.println(que.queue[i]);
+        }
+
+        que.deque();
+        que.deque();
+        que.deque();
+        que.deque();
+        //que.deque();
+        for(int i = que.front; i<que.rear; i++){
+            System.out.println(que.queue[i]);
+        }
+
+    }
+}
